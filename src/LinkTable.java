f4b3bf951a580d6c05eb74ce6e21a3bb4161e84b
Eric Chen <xc12lw@brocku.ca>
2018-10-27 17:14:25 -0400

    Add LinkTable

---
+++ src/LinkTable.java
@@ -0,0 +1,19 @@
+public class LinkTable {
+
+    private boolean links[][];
+
+    public LinkTable(int numMachines){
+        links = new boolean[numMachines][numMachines];
+    }
+
+    public boolean isConnected(String address_1, String address_2){
+        //TODO: check if two machines are connected
+        return false;
+    }
+
+    public void connect(String address_1, String address_2){
+        //TODO: connect two machines and record it in the link table
+    }
+
+
+}
